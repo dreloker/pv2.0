630bb7e87adcde657b1358e0414e1c7051d69e92
J.M.E <joaquin.munoz91@gmail.com>
2025-06-21 10:11:45 -0400

    logo y navbar que se revelan, ok. siguiente: caja de servicios con imágenes

--- static/js/script.js
+++ static/js/script.js
@@ -9,4 +9,35 @@
     document.querySelector(".intro-port").scrollIntoView({ behavior: "smooth" });
   });
 
-  
+   // salto "index" 
+  document.querySelector(".saltoindex").addEventListener("click", () => {
+    document.querySelector(".intro-index").scrollIntoView({ behavior: "smooth" });
+  });
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.querySelector(".isologoheader svg");
+    const saltoindex = document.querySelector(".saltoindex");
+    const introPort = document.querySelector(".intro-port");
+
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            logo.style.opacity = "1";
+            saltoindex.style.opacity = "1";
+          } else {
+            logo.style.opacity = "0";
+            saltoindex.style.opacity = "0";
+          }
+        });
+      },
+      {
+        threshold: 0.5 // cambia según cuánta parte de .intro-port debe estar visible
+      }
+    );
+
+    if (introPort) {
+      observer.observe(introPort);
+    }
+  });
